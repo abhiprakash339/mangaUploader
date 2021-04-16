1a7e7cb64b3164e0aa9f9483a05d41d288844a0d
abhishek <1289prakash@gmail.com>
2021-04-17 01:14:53 +0530

    added caption ti file length greater than 40

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-pdf_name = 'Kaguya Wants To Be'
+# pdf_name = 'Kaguya Wants To Be Confessed To'
+pdf_name = 'Boku no hero Academia'
 chapter = 2
 a = pdf_name + " Chapter " + str(chapter).zfill(3) + ".pdf"
 print(len(a))
